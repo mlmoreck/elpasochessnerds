3a03784194090af45cbaf29f0852d8c16d5d0bf1
marymoreck <email@example.com>
2025-06-28 11:57:23 -0600

    Now routes to info pages accordingly and flips menu back and forth.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
   $('.flip-card').on('click', function(){
-    if($(this).hasClass('flipped')){
-      $(this).removeClass('flipped');
-    } else {
-      $(this).addClass('flipped');
+    $this = $(this);
+
+    if(!$this.hasClass('flipped')){ // not flipped yet
+      $this.addClass('flipped');
+    } else if(!$this.hasClass('menu')){ // flipped info card; go to full page
+      window.location = './' + $this.data('page') + '.html';
+    } else { // menu/other; just flip it back
+      $this.removeClass('flipped');
     }
   })
 });
